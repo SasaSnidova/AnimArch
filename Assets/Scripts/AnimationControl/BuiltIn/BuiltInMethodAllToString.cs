fdc35aba40a846266b96cc5324a804e8323ba2a2
Neltharion59 <lukawh5959@gmail.com>
2026-02-23 14:13:58 +0100

    [FEATURE] Enable ToString method

---
+++ Assets/Scripts/AnimationControl/BuiltIn/BuiltInMethodAllToString.cs
@@ -0,0 +1,29 @@
+﻿using OALProgramControl;
+using System;
+using System.Collections.Generic;
+
+namespace Assets.Scripts.AnimationControl.BuiltIn
+{
+    public class BuiltInMethodAllToString : BuiltInMethodBase
+    {
+        public override EXEExecutionResult Evaluate(EXEValueBase owningObject, List<EXEValueBase> parameters)
+        {
+            string stringValue;
+            try
+            {
+                VisitorCommandToString visitor = new VisitorCommandToString();
+                owningObject.Accept(visitor);
+                stringValue = visitor.GetCommandString();
+                stringValue = $"\"{stringValue}\"";
+            }
+            catch (Exception e)
+            {
+                return EXEExecutionResult.Error("XEC2059", string.Format($"Failed to convert value to string because {e.Message}."));
+            }
+
+            EXEExecutionResult result = EXEExecutionResult.Success();
+            result.ReturnedOutput = new EXEValueString(stringValue);
+            return result;
+        }
+    }
+}
